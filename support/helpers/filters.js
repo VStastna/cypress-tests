bfdc56d3a1637709e90b667cf2fc74a331f90608
vstastna <stastna.veronika@yahoo.co.uk>
2024-02-22 15:16:48 +0100

    further refactoring

--- support/helpers/filters.js
+++ support/helpers/filters.js
@@ -7,21 +7,21 @@ const hasMaxPassengers = (maxPassengers, booking) => booking.passengers.length <
 )
 const hasNoPassportDetails = booking =>
     booking.passengers.every(passenger => passenger.document_number === null)
-const getCheckInApaxiWithoutPassportDetailsSubmitted = booking =>
-    hasCheckInStatus('provide_apaxi', booking) && hasNoPassportDetails(booking)
-const getPlusInsurance = booking => hasInsuranceLevel('travel_plus', booking)
+const getCheckInWithoutPassportDetailsSubmitted = booking =>
+    hasCheckInStatus('provide_ap', booking) && hasNoPassportDetails(booking)
+const getPlusInsurance = booking => hasInsuranceLevel('plus', booking)
 const getSinglePassenger = booking => hasMaxPassengers(1, booking)
 const getWithNonUsPassengers = booking =>
     booking.passengers.every(passenger => passenger.nationality !== 'us')
 export const FILTERS = {
-    CHECK_IN_APAXI_WITHOUT_PASSPORT_DETAILS_SUBMITTED: 'checkInApaxiWithoutPassportDetailsSubmitted',
+    CHECK_IN_WITHOUT_PASSPORT_DETAILS_SUBMITTED: 'checkInApaxiWithoutPassportDetailsSubmitted',
     NON_US_PASSENGERS: 'nonUsPassengers',
     PLUS_INSURANCE: 'plusInsurance',
     SINGLE_PASSENGER: 'singlePassenger',
 }
 const filters = {
-    [FILTERS.CHECK_IN_APAXI_WITHOUT_PASSPORT_DETAILS_SUBMITTED]:
-    getCheckInApaxiWithoutPassportDetailsSubmitted,
+    [FILTERS.CHECK_IN_WITHOUT_PASSPORT_DETAILS_SUBMITTED]:
+    getCheckInWithoutPassportDetailsSubmitted,
     [FILTERS.NON_US_PASSENGERS]: getWithNonUsPassengers,
     [FILTERS.PLUS_INSURANCE]: getPlusInsurance,
     [FILTERS.SINGLE_PASSENGER]: getSinglePassenger,
